cd9bbdab722b0484eff266c4dd66b2b452ac3e69
devwithu <devwithu@naver.com>
2020-02-02 15:32:30 +0900

    Initial Commit

---
+++ Wheather-Forecast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wheather-Forecast
+//
+//  Created by jdj on 2020/02/02.
+//  Copyright © 2020 jdj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
